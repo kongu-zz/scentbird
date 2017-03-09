984fb3a7f1c1eaae025301b0e5ebd205457ce0e0
Gennady Konovalov <kongu@mail.ru>
2017-03-09 13:30:59 +0300

    начал разделять блоки верстки на компоненты

--- js/PaymentPage/Components/PaymentPageLayout.jsx
+++ js/PaymentPage/Components/PaymentPageLayout.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import {PaymentPageForm} from "./PaymentPageForm";
+import classNames from "classnames";
+
+const ContentHeader = ({title, isDesktop = false}) => (
+    <div class={classNames("content-header", {"content-header-desktop": isDesktop})}>
+        <div class="title">
+            {title}
+        </div>
+        <div class="slogan">
+            Billed monthly. Renews automatically, cancel any time. Free shipping.
+        </div>
+    </div>
+);
 
 export class PaymentPageLayout extends React.Component {
     constructor(props) {
@@ -8,13 +20,109 @@ export class PaymentPageLayout extends React.Component {
     }
 
     componentDidMount() {
-
     }
 
     render() {
-
         return (
-            <PaymentPageForm/>
+            <div class="container">
+
+                <div class="wrapper">
+                    <div class="header">
+                        <div class="logo"></div>
+                    </div>
+                    <ContentHeader title="MONTHLY SUBSCRIPTION"/>
+                    {/*<div class="content-header">
+                        <div class="title">
+                            MONTHLY SUBSCRIPTION
+                        </div>
+                        <div class="slogan">
+                            Billed monthly. Renews automatically, cancel any time. Free shipping.
+                        </div>
+                    </div>*/}
+                    <div class="aside">
+                        <div class="order">
+                            <div class="free-perfume-product"/>
+                            <div class="total-body">
+                                <table>
+                                    <colgroup>
+                                        <col/>
+                                        <col/>
+                                    </colgroup>
+                                    <tbody>
+                                    <tr>
+                                        <td>Monthly subscription</td>
+                                        <td>$14.95</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Shipping</td>
+                                        <td>FREE</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tax</td>
+                                        <td>$2.35</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Discount</td>
+                                        <td class="discount">-$5</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Credit (Balance $100)</td>
+                                        <td>$50</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="total-final">
+                                <table>
+                                    <colgroup>
+                                        <col/>
+                                        <col/>
+                                    </colgroup>
+                                    <tbody>
+                                    <tr>
+                                        <td>TOTAL</td>
+                                        <td>$25.00</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="coupon-code">Have a <a href="#">coupon code</a>?</div>
+                        </div>
+                        <div class="picture picture-desktop">
+                            <div class="bird-on-bike"></div>
+                            <p>
+                                You will receive an email confirmation when recipient accepts your gift.
+                                Scentbird ships between the 15th and the 18th of every month.
+                                Recipient will receive an email confirmation of shipment every month.
+                                Please allow 5-7 days for delivery.
+                            </p>
+
+                        </div>
+                    </div>
+                    <div class="content">
+                        <ContentHeader title="MONTH-TO-MONTH SUBSCRIPTION" isDesktop={true}/>
+                        {/*<div class="content-header content-header-desktop">
+                            <div class="title">
+                                MONTH-TO-MONTH SUBSCRIPTION
+                            </div>
+                            <div class="slogan">
+                                Billed monthly. Renews automatically, cancel any time. Free shipping.
+                            </div>
+                        </div>*/}
+                        <PaymentPageForm/>
+                    </div>
+                    <div class="picture">
+                        <p>
+                            You will receive an email confirmation when recipient accepts your gift.
+                            Scentbird ships between the 15th and the 18th of every month.
+                            Recipient will receive an email confirmation of shipment every month.
+                            Please allow 5-7 days for delivery.
+                        </p>
+                        <div class="bird-on-bike"></div>
+                    </div>
+                    <div class="cleared"></div>
+                </div>
+            </div>
         );
     }
 }
